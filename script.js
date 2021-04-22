2758cef048d8454d95438dce2a6a9f433777682b
Kirill Jin <topstar0527@outlook.com>
2021-04-22 10:40:48 -0800

    first update

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+var slideIndex = 0;
+
+
+ 
+ 
+showSlides();
+
+function showSlides() {
+  
+   var slides = document.getElementsByClassName("mySlides"); 
+   if(slideIndex==slides.length)slideIndex=0;
+ for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  console.log(slides.length)
+  
+  slideIndex++;
+  slides[slideIndex-1].style.display = "block";
+  setTimeout("showSlides()", 4000); // Change image every 2 seconds
+}
